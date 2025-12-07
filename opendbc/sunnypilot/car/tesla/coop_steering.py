4e1adaa56ae2f0ae610e9e4885d10f32b6bd23d0
dzid26 <dzidmail@gmail.com>
2025-12-07 22:11:20 +0000

    disable filtering during override

--- opendbc/sunnypilot/car/tesla/coop_steering.py
+++ opendbc/sunnypilot/car/tesla/coop_steering.py
@@ -323,7 +323,7 @@ class CoopSteeringCarController:
     apply_angle = self.resume_steer_desired_rate_limit(lat_active, apply_angle, steeringAngleDegPhaseLead)
 
     if angle_coop_enabled:
-      apply_angle = self.overriding_steer_desired_accel_limit(lat_active, apply_angle, CS.out.vEgo, CS.out.steeringTorque)
+      # apply_angle = self.overriding_steer_desired_accel_limit(lat_active, apply_angle, CS.out.vEgo, CS.out.steeringTorque)
       self.debug_angle_desired_limited = apply_angle #! debug
       apply_angle = self.apply_override_angle(lat_active, apply_angle, CS.out.steeringTorque, CS.out.vEgo, VM)
       if not low_speed_pause_enabled:
