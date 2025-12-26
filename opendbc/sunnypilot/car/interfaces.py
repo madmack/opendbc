184d47a5bc6e207eaabe72a91fc9664dc87367c1
madmack <alali48@gmail.com>
2025-12-26 09:01:08 +0300

    make default tesla coop 1 just in case

--- opendbc/sunnypilot/car/interfaces.py
+++ opendbc/sunnypilot/car/interfaces.py
@@ -104,7 +104,7 @@ def _initialize_custom_longitudinal_tuning(CI, CP: structs.CarParams, CP_SP: str
 def _initialize_coop_steering(CP: structs.CarParams, CP_SP: structs.CarParamsSP,
                               params_dict: dict[str, str]) -> None:
   if CP.brand == 'tesla':
-    coop_steering = int(params_dict.get("TeslaCoopSteering", 0)) == 1
+    coop_steering = int(params_dict.get("TeslaCoopSteering", 1)) == 1
     if coop_steering:
       CP_SP.flags |= TeslaFlagsSP.COOP_STEERING.value
     lkas_steering = int(params_dict.get("TeslaLkasSteering", 0)) == 1
